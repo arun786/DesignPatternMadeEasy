d12e851e406839bcc216c74f4a80ec620c1c43e5
Adwiti789 <adwiti1975@gmail.com>
2019-05-21 23:17:33 -0700

    Liskov Substitution principle

--- src/main/java/SOLID/LiskhovSubstitutionPrinciple.java
+++ src/main/java/SOLID/LiskhovSubstitutionPrinciple.java
@@ -1,4 +1,67 @@
 package SOLID;
 
+import lombok.*;
+
+/**
+ * Object of Parent class can be replaced with object of child class without any negative side effects
+ */
 public class LiskhovSubstitutionPrinciple {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2, 3);
+        Demo.useIt(rectangle);
+
+        Rectangle square = new Square(2);
+        Demo.useIt(square);
+    }
+
 }
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+class Rectangle {
+    protected int width;
+    protected int height;
+
+    public int getArea() {
+        return this.width * this.height;
+    }
+}
+
+
+@NoArgsConstructor
+class Square extends Rectangle {
+
+    public Square(int size) {
+        this.height = this.width = size;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+}
+
+class Demo {
+    static void useIt(Rectangle rectangle) {
+
+        int width = rectangle.getWidth();
+        rectangle.setHeight(10);
+
+        System.out.println("Expected area = " + width * 10 + " actual output will be " + rectangle.getArea());
+    }
+}
+
+
+
