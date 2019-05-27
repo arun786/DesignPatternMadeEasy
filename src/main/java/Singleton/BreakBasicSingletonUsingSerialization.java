39851dad7057d160c641450ebe9821664c313158
Adwiti789 <adwiti1975@gmail.com>
2019-05-26 17:22:05 -0700

    readResolve

--- src/main/java/Singleton/BreakBasicSingletonUsingSerialization.java
+++ src/main/java/Singleton/BreakBasicSingletonUsingSerialization.java
@@ -51,6 +51,12 @@ class Singlet implements Serializable {
     }
 
     /**
+     * The readResolve method is called when ObjectInputStream has read an object from the stream
+     * and is preparing to return it to the caller. ObjectInputStream checks whether the class of
+     * the object defines the readResolve method. If the method is defined, the readResolve method
+     * is called to allow the object in the stream to designate the object to be returned.
+     * The object returned should be of a type that is compatible with all uses. If it is not compatible,
+     * a ClassCastException will be thrown when the type mismatch is discovered.
      * @return - In order to make the singleton a singleton, we need to return the instance for the below method
      */
     protected Object readResolve() {
